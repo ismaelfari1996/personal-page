58b7a68ba31287c2762a1d07c229f2eddce62784
ismaelfari1996 <abel_fari@hotmail.com>
2022-02-04 22:03:02 -0500

    commit

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,3 +1,4 @@
+import SocialNetwork from "../../configfiles/social-network.json";
 export default function MainBody() {
     return (
         <div id="home" className="title bg-transparent bgstyle text-light min-vh-100 d-flex align-content-center align-items-center flex-wrap m-0 " 
@@ -10,21 +11,17 @@ export default function MainBody() {
                     Apasionado por la tecnologia y la programacion.
                 </div>
                 <div className="p-5">
-                    <a className="p-2" target="_blank" rel="noopener noreferrer" href="https://github.com/ismaelfari1996" aria-label="My github">
-                        <i className="fab fa-github  fa-3x social-icons"></i>
-                    </a>
-                    <a className="p-2" target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/abel.farianago" aria-label="My facebook">
-                        <i className="fab fa-facebook  fa-3x social-icons"></i>
-                    </a>
-                    <a className="p-2"  target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/ismaelfari96/" aria-label="My instagram">
-                        <i className="fab fa-instagram  fa-3x social-icons"></i>
-                    </a>
-                    <a className="p-2"  target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/abel-farinango-40021321a/" aria-label="My linkedin">
-                        <i className="fab fa-linkedin  fa-3x social-icons"></i>
-                    </a>
-                    <a className="p-2"  target="_blank" rel="noopener noreferrer" href="https://www.twitter.com/hashirshoaeb/" aria-label="My twitter">
-                        <i className="fab fa-twitter  fa-3x social-icons"></i>
-                    </a>
+                    {
+                        SocialNetwork.map((social, index) => {
+                            return (
+                            <a className="p-2" target="_blank" rel="noopener noreferrer" href={social.url} aria-label={"My "+social.name}>
+                                <i className={social.icon}></i>
+                            </a>    
+                            );
+                        })
+
+                    }
+
                 </div>
                 <a className="btn btn-outline-light btn-lg " href="#about" role="button" aria-label="Learn more about me">
                     Mas sobre mi
